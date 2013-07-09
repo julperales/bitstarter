4e2895ce4f01cd5b036ad3d2343f1edd31e68f84
Julio de los Perales <todaytogether@hotmail.com>
2013-07-09 22:27:39 +0000

    web.js from file

--- web.js
+++ web.js
@@ -1,12 +1,26 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+	var fs = require("fs");
+
+
+	var buffer = new Buffer (256, 'utf8');
+
+
+	content_buffer = fs.readFileSync('index.html', 'utf8');
+	content = buffer.toString('utf8', content_buffer);
+
+	response.send(content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
